7e788fd93d030e230c2ab90d21553fb56cb64119
Jessamine Brown <brownjessa11@gmail.com>
2020-08-09 18:31:21 -0500

    post & put html route

---
+++ Develop/routes/html_routes.js
@@ -0,0 +1,21 @@
+const Workout = require("../models/workout");
+const router = require("express").Router();
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.put("/apiworkouts/", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
