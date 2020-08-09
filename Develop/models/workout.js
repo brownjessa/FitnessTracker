8bee1fe2bf9aa7991920408c310e4f9d9358dbad
Jessamine Brown <brownjessa11@gmail.com>
2020-08-09 18:09:57 -0500

    finish workout.js

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -2,13 +2,32 @@ const Schema = mongoose.Schema;
 const mongoose = require("mongoose");
 
 const WorkoutSchema = new Schema({
-  day: {},
-  exercises: {},
-  time: {},
-  weight: {},
-  reps: {},
-  sets: {},
-  distance: {},
+  day: {
+      type: Date,
+      default: Date.now
+  },
+  exercises: {
+      type: String,
+      required: "What kind of exercise?"
+  },
+  name: {
+      type: "What is the name of the exercise?"
+  }
+  time: {
+      type: Number,
+  },
+  weight: {
+      type: Number,
+  },
+  reps: {
+      type: Number,
+  },
+  sets: {
+      type: Number,
+  },
+  distance: {
+      type: Number,
+  },
 });
 
 const Workout = mongoose.model("workout",, workoutSchema);
