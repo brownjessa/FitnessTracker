428fded0cd22dd887523d25db97efa6c294555fb
Jessamine Brown <brownjessa11@gmail.com>
2020-08-09 18:44:52 -0500

    create html routes post, put, get, delete

--- Develop/routes/html_routes.js
+++ Develop/routes/html_routes.js
@@ -2,20 +2,29 @@ const Workout = require("../models/workout");
 const router = require("express").Router();
 
 router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+  Workout.create(body).then((dbWorkout) => {
+    res.json(dbWorkout);
+  });
 });
 router.put("/apiworkouts/", (req, res) => {
-  Workout.find({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
+  Workout.find({}).then((dbWorkout) => {
+    res.json(dbWorkout);
+  });
+});
+router.get("/api/workouts", (req, res) => {
+  Workout.find({}).then((dbWorkout) => {
+    res.json(dbWorkout);
+  });
+  router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .limit(30)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      });
+    router.delete("/api/workouts", ({ body }, res) => {
+      Workout.findByIdAndRemove(body.id).then(() => {
+        res.json(true);
+      });
     });
+  });
 });
